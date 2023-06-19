bdd099c172b88a2deda8a752ff41a700aee86dac
Minaadel99 <menakingking22@gmail.com>
2023-06-19 10:46:09 -0700

    variable

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: prints  alphabet in lowercase and in uppercase
+ * Description: prints all alphabet except q and e
  * Return: Always 0 (success)
 */
 int main(void)
 {
 char ch = 'a';
-char CH = 'A';
 while (ch <= 'z')
 {
+if (ch == 'e' || ch == 'q')
+ch++;
 putchar(ch);
 ch++;
-}
-while (CH <= 'Z')
-{
-putchar(CH);
-CH++;
 }
 putchar('\n');
 return (0);
