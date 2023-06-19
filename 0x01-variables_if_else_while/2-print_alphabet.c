93e5f2a38272c778d8d2517bcf1c10b21e5fad55
Minaadel99 <menakingking22@gmail.com>
2023-06-19 09:09:29 -0700

    variable c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphbet
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+        int i;
+        char alp[26] = "abcdefghigklmnopqrstuvwxyz";
+        for (i = 0; i <26; i++)
+        {
+                putchar(alp[i]);
+        }
+        putchar('\n')
+        return (0);
+}
