581e3d9541332b64974bee99fa4097a5e667d190
Minaadel99 <menakingking22@gmail.com>
2023-06-21 09:30:33 -0700

    var

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _abc - function that computes the absolute value of an integer
+ * _abs - function that computes the absolute value of an integer
  * @n: takes in integer type input for function
  * Return: Always 0 (success)
 */
