3ba68b0f59ad0fe36aa0f3886395c7111adfd876
Minaadel99 <menakingking22@gmail.com>
2023-06-21 10:31:22 -0700

    var

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_to_98 - print n to 98 counts separated by comma, followed by space and number
+ * print_to_98 - print n to 98 counts separated by comma, followed by space
  * @n: input
 */
 void print_to_98(int n)
