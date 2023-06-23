6138b2ec3082a0d127b3952b23c838f5150a60d9
Minaadel99 <menakingking22@gmail.com>
2023-06-23 10:28:27 -0700

    c 4

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - print a straight line
+ * @n: is the number of times the _ character
+*/
+void print_line(int n)
+{
+	int lncahr;
+	if (n<= 0)
+		_putchar('\n');
+	else
+	{
+		for (lnchar = 1; lnchar <= n; lnchar++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
