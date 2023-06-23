ed559a23059a9d7479badfa5cd3c051f6c42a13d
Minaadel99 <menakingking22@gmail.com>
2023-06-23 09:21:59 -0700

    c 4

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - check if c is upper
+ * @c: input foralphabet
+ * Return: 1 if its uppercase, 0 if not
+*/
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		retrn (0);
+}
