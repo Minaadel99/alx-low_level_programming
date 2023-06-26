b16f6ca4f4e80c33f4bbba831d326923838195cc
Minaadel99 <menakingking22@gmail.com>
2023-06-26 16:44:31 -0700

    read

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 /**
- * _putchar - writes the character c to stdout
+ * _putchar - writes the character cto stdout
  * @c: the character to print
- * Return: on success 1.
- * on error, -1 is returned, and errno is set appropriately.
- */
+ * Return: om success 1
+ * on error, -1 is returned
+*/
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
